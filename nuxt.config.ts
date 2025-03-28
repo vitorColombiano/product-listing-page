5a9d651aa335b759dcfb667adca852f66f6b94b6
vitorColombiano <vicolombiano69@gmail.com>
2025-03-28 13:48:13 -0300

    add Tailwind CSS and PostCSS configuration to Nuxt.js application

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  build: {
+    transpile: ['tailwindcss'],
+  },
+
+  compatibilityDate: '2025-03-28',
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+    './plugins/**/*.{js,ts,jsx,tsx}',
+    './nuxt.config.{js,ts}',  // Incluir o arquivo de configuração do Nuxt
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
